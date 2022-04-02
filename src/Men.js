3758eefc70390028d66cb68b300e324adb7e72cf
ntykwok <85721398+ntykwok@users.noreply.github.com>
2022-04-02 16:13:38 +0800

    firebase

---
+++ src/Men.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Men() {
+    return (
+        <div>
+            <h1>This is the men page</h1>
+        </div>
+    )
+}
+
+export default Men
---
+++ src/Women.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Women() {
+    return (
+        <div>
+            <h1>This is the women page</h1>
+        </div>
+    )
+}
+
+export default Women
