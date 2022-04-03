8a1c12a08eeddc9ec4e6211dd75b2cf69f0b8103
ntykwok <85721398+ntykwok@users.noreply.github.com>
2022-04-03 20:49:46 +0800

    all

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Home from "./Home"
 import Men from "./Men"
 import Women from "./Women"
 import CheckOut from "./CheckOut"
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
 function App() {
     return (
@@ -19,3 +21,19 @@ function App() {
   }
   
   export default App
+
+  // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC85be_EMmnCHhKU34jFYerMtl3pUC5Emo",
+  authDomain: "athletico-shop.firebaseapp.com",
+  projectId: "athletico-shop",
+  storageBucket: "athletico-shop.appspot.com",
+  messagingSenderId: "753795811389",
+  appId: "1:753795811389:web:0ed44000d14be28defc254",
+  measurementId: "G-W4PS0NLERW"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
